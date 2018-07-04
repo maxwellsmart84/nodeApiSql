464de343c943ab6fa8d1896f3af28e230240ee39
Maxwell Krause <maxwell.l.krause@gmail.com>
2018-07-04 09:54:09 -0400

    added correct disconnect pattern

--- src/serve.js
+++ src/serve.js
@@ -5,15 +5,10 @@ import knex from 'knex';
 
 dotenv.config();
 
-// More customized configurations here:  https://knexjs.org/#Installation-client
-const sql = knex.connection({
-  client: process.env.SQL_CLIENT,
-  connection: sqlConnectionConfig(process.env.SQL_CLIENT),
-});
 
 function sqlConnectionConfig(sqlClient) {
   if (sqlClient === 'sqlite3') {
-    return { filename: process.env.DB_HOST, };
+    return { filename: process.env.DB_HOST };
   }
   return {
     host: process.env.DB_HOST || '127.0.0.1',
@@ -23,19 +18,42 @@ function sqlConnectionConfig(sqlClient) {
   };
 }
 
+// More customized configurations here:  https://knexjs.org/#Installation-client
+const sql = knex.connection({
+  client: process.env.SQL_CLIENT,
+  connection: sqlConnectionConfig(process.env.SQL_CLIENT),
+});
+
+
+// Create the database
+sql();
+sql.raw(`CREATE DATABASE ${process.env.DB_NAME}`)
+  .then(() => {
+    sql.destroy();
+  });
+// reconnect
 sql();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true, }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.status(200).send({ server: 'online', });
+  res.status(200).send({ server: 'online' });
 });
 
 app.listen(PORT, () => {
-  console.info(`Dev server started on port ${PORT}! Happy coding!`);
+  console.info(`Web server started on port ${PORT}! Happy coding!
+  `);
+});
+
+process.on('SIGINT', () => {
+  app.close(() => {
+    console.error('Program has unexpectedly stopped. Gracefully disconnecting from db and stopping server');
+    sql.destroy();
+    process.exit(1);
+  });
 });
 
 // for tests
